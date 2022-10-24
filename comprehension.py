9ff2d4eeb57dc66972a67eb012b42a337d56857c
Daniel <danielmontrest@gmail.com>
2022-10-24 11:17:50 -0500

    Mensaje

--- comprehension.py
+++ comprehension.py
@@ -1,1 +1,12 @@
 # Comprehensions son listas, diccionarios y conjuntos creados a partir de un cilo for y en una sola linea de comando
+
+
+#List comprehensions ejemplos
+
+
+numbers = [i for i in range(1, 11)]
+print(numbers)
+
+
+numbers2 = [i * 2 for i in range (1, 11)]
+print(numbers2)
