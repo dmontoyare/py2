808a8bfceb94d848ba9a967fba7cc46aa5161eed
Daniel <danielmontrest@gmail.com>
2022-10-24 12:04:21 -0500

    Se agrega archivo readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # py2
+
+
+Repositiorio del los ejercicios del curso curso:
+Curso de Python: Comprehensions, Funciones y Manejo de Errores
+Platzi.com
+Profesor: Nicolas Molina
+Fecha de inicio: 2022/10/24
--- comprehension.py
+++ comprehension.py
@@ -1,12 +1,22 @@
 # Comprehensions son listas, diccionarios y conjuntos creados a partir de un cilo for y en una sola linea de comando
 
-
 #List comprehensions ejemplos
-
-
 numbers = [i for i in range(1, 11)]
 print(numbers)
 
-
+# Al elemento "i" se puede ver modificado matematicamente
 numbers2 = [i * 2 for i in range (1, 11)]
 print(numbers2)
+
+# Se pueden agregar condicionales 
+numbers3 = [i for i in range(1, 11) if i % 2 == 0]
+print(numbers3)
+
+# Diccionarios igual que las lsitas, obviamente lleva clave y valor 
+dict1 = {i: i*2 for i in range(1, 11)}
+print(dict1)
+
+# Diccionario a partir de una lista 
+list = ["Daniel", "Maria", "Juan", "Ramiro"]
+dict3 = {i: i[0:1] for i in list}
+print(dict3)
